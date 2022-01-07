d42873a08c0baf7f300eebcaabaaab1818f915d7
9IcTpy6 <9i.c.tpy6@gmail.com>
2022-01-07 02:32:54 +0200

    NavBar
    Navbar

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
-
+import Navbar from './components/Navbar';
+ 
 const App = () => {
   return (
     <div className="App">
+      <ToastContainer />
+        <Navbar />
       <h1>Welcome to Contact Book</h1>
     </div>
   );
