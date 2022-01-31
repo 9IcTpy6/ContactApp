c712c55abfc79e25697c310051e972546f8b2d7a
9IcTpy6 <9i.c.tpy6@gmail.com>
2022-01-31 18:06:50 +0200

    Add Add
    Add Contact

--- project/src/App.js
+++ project/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import AddContact from './components/AddContact';
  
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
       <Navbar />
       <Switch>
           <Route exact path ="/" component={()=><Home/> } />
-          <Route path ="/add"><h1>i am add component</h1></Route>
+          <Route path ="/add"><AddContact/></Route>
           <Route  path ="/edit/:id"><h1>i am edit component</h1> </Route>
       </Switch>
       
