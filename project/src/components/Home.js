f7cbc2c3830cbefa3fce95f9b83119cd90e6e844
9IcTpy6 <9i.c.tpy6@gmail.com>
2022-02-24 02:21:41 +0200

    Read Contact
    ReadContact on home page

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -1,15 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+
+    const contacts = useSelector(state=>state);
+
+
     return (
         <div className="container">
         <div className="row">
             <div className="col-md-12 my-5 text-right">
                 <Link to="/add" className="btn btn-outline-dark">Add Contact</Link>
             </div>
-            <div className="col-md-6 mx-auto">
-            <h1>Wellcome to Home component</h1>
+            <div className="col-md-10 mx-auto">
+            <table className="table table-hover">
+                <thead className="text-white bg-dark text-center">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Number</th>
+                        <th scope="col">action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        contacts.map((contact, id)=>(
+                            <tr key={id}>
+                                <td>{id + 1}</td>
+                                <td>{contact.name}</td>
+                                <td>{contact.email}</td>
+                                <td>{contact.number}</td>
+                                <td>
+                                    <Link to={`/edit/${contact.id}`} className="btn btn-small btn-primary mr-2">Edit</Link>
+                                    <button type="button" className="btn btn-small btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
         </div>
         </div>
